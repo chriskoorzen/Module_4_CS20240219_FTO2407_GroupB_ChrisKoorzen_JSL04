c340d67edd13e2fd00843aa2e455373b42a958f1
Chris Koorzen <fckoorzen@gmail.com>
2024-08-28 11:37:41 +0200

    Be strict, add hard challenge.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";       // "Be tolerant with others, and strict with yourself." - Marcus Aurelius
+
 // # [JSL04] Submission: Monster Ternary Operator
 
 // #### Challenge 1: Flavor Selection
@@ -7,7 +9,8 @@
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
 let preferenceChoice = userPreference==='Sugar-free' ? 'Sugar-free': 'Regular';
 console.log("User's Preference:", userPreference);
-console.log("What we heard:", preferenceChoice, "\n\n");
+console.log("What we heard:", preferenceChoice);
+console.log("\n")
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
@@ -16,7 +19,8 @@ console.log("What we heard:", preferenceChoice, "\n\n");
 let cansLeft = 3; // Any number of cans
 let stockAction = cansLeft >= 5 ? "We're stocked!" : "Time to restock!";
 console.log("Cans Left", cansLeft);
-console.log("Action:", stockAction, "\n\n");
+console.log("Action:", stockAction);
+console.log("\n")
 
 
 // #### Challenge 3: Workout Intensity
@@ -26,7 +30,8 @@ console.log("Action:", stockAction, "\n\n");
 let heartRate = 95; // Current heart rate in bpm
 let drinkRecommendation = heartRate < 100 ? "Boost needed!" : "Energy levels are high!";
 console.log("User's heartrate", heartRate);
-console.log("Our recommendation:", drinkRecommendation, "\n\n");
+console.log("Our recommendation:", drinkRecommendation);
+console.log("\n")
 
 
 // #### Challenge 4: Temperature Suitability
@@ -36,7 +41,8 @@ console.log("Our recommendation:", drinkRecommendation, "\n\n");
 let currentTemp = 4; // Current temperature in °C
 let serveAction = currentTemp > 5 ? "Needs a cooler!" : "Chilled to perfection!";
 console.log("Current temp:", currentTemp);
-console.log("Is it ready to serve:", serveAction, "\n\n");
+console.log("Is it ready to serve:", serveAction);
+console.log("\n")
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -50,14 +56,31 @@ let currentHour = 22;
 // Note: in 24-hour format, 23 is the highest hour.
 //   Because midnight is 0 (zero) and is the upper limit to this check,
 //   we only have to check if its earlier than 7am.
-//
-//   (Note to admin: if you want to make this "complicated", you should choose a time like 7am - 10pm)
 currentHour > 23 ? console.log("Invalid hour for 24-hour format") : null;
 
+//   (Note to admin: if you want to make this "complicated", you should choose a time like 7am - 10pm)
 let drinkAction = currentHour < 7 ? "Better stick to water." : "Unleash the beast!";
 console.log("Current hour:", currentHour);
-console.log("Should we drink:", drinkAction, "\n\n");
+console.log("Should we drink:", drinkAction);
+console.log("\n")
 
 
-// Wish there was some complex ones.
+// Lets try a harder one
+console.log("Hard Mode:")
+
+// No caffeine before 8am. May have before 12pm. Nothing again till 3pm. Ok until 7pm. No more than 2 per day.
+let currentTime = 13;
+let totalDrinks = 1;
+let hydrateStrategy = totalDrinks >= 2 ? "Better stick to water." :
+                      (currentTime > 8 && currentTime < 12) ? "Unleash the beast!" :
+                      (currentTime > 15 && currentTime < 19) ? "Unleash the beast!" :
+                      "Better stick to water."
+
+// The time condition can be "simplified" to:
+// (currentTime > 8 && currentTime < 12) || (currentTime > 15 && currentTime < 19) ? "Unleash the beast!" : "Better stick to water."
+
+console.log("Current hour:", currentTime);
+console.log("Total drinks today:", totalDrinks);
+console.log("Hydration tactic:", hydrateStrategy, "\n\n");
+
 // Check this out: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator#conditional_chains
